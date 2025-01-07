bb105216649d9dc7df50edf4cd47da5832d2fdac
Alexandre Leote <alexandreleoteweb@gmail.com>
2025-01-07 16:44:56 +0100

    📦 NEW: Ajout des formulaires aux entités associées

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +20,19 @@ class EmployeController extends AbstractController
         return $this->render('employe/index.html.twig', [
             'employes' => $employes,
             'description' => 'Liste des employés'
+        ]);
+    }
+
+    #[Route('/employe/new', name: 'new_employe')]
+    public function new(Request $request): Response
+    {
+        $employe = new Employe();
+
+        $form = $this->createForm(EmployeType::class, $employe);
+
+        return $this->render('employe/new.html.twig', [
+            'formAddEmploye' => $form,
+            'description' => 'Création d’une nouvelle employé'
         ]);
     }
 
